dc3b8e03c4ca2e11f5fddf0eaa288f3ce364fb32
Daniel <itsnidhogg.13@gmail.com>
2024-04-19 10:57:08 -0500

    Database configuration, signup.php

--- src/backend/signup.php
+++ src/backend/signup.php
@@ -1,11 +1,17 @@
-<?php
-$fullname = $_POST['fname'];
-$email = $_POST['email'];
-$passwd = $_POST['passwd'];
-$enc_passwd = md5($passwd);
+<?php 
+include('../../config>/database.php');
+$fullname=$_POST['fname'];
+$email=$_POST['email'];
+$passwd=$_POST['passwd'];
+$enc_pass=md5($passwd);
 
-echo "Your fullname: ". $fullname. "<br";
-echo "Your email: ". $email. "<br>";
-echo "Your password: ". $passwd;
-echo "Your password enc: ". $enc_passwd;
+$sql = "INSERT INTO users (fullname, email, password) VALUES('$fullname', '$email','$enc_pass')";
+$ans = pg_query($conn,$sql);
+if($ans){
+    echo"user has been created successfully";
+}else{
+    echo "error: ".preg_last_error();
+}
+//close conection
+pg_close($conn)
 ?>
